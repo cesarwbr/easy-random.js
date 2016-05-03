169a444cd009a008bf3b96f8521a19a847f59231
cesarwbr <cesarwbr@gmail.com>
2016-05-03 10:32:08 -0300

    Add choice and shuffle

--- random.js
+++ random.js
@@ -9,10 +9,38 @@ module.exports = {
   },
   nextInt: function(max) {
     if(!max) {
-      console.error('EASY RANDOM: You should pass a max argument.');
+      console.error('EASY RANDOM: You must pass a max argument.');
       return;
     }
 
     return Math.floor(Math.random() * (max));
+  },
+  choice: function(seq) {
+    if (typeof seq === 'string' || seq instanceof Array) {
+      return seq[this.nextInt(seq.length - 1)];
+    }
+
+    console.error('EASY RANDOM: You must pass a sequence of Array or String as argument.');
+  },
+  shuffle: function(seq) {
+    if (!(seq instanceof Array)) {
+      console.error('EASY RANDOM: You must pass a sequence of Array as argument.');
+      return;
+    }
+
+    var counter = seq.length;
+    var index, temp;
+
+    while (counter > 0) {
+        index = Math.floor(Math.random() * counter);
+
+        counter--;
+
+        temp = seq[counter];
+        seq[counter] = seq[index];
+        seq[index] = temp;
+    }
+
+    return seq;
   }
 };
