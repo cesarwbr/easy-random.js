9dda7bf5b99205df4f7ab003e0ad1209c4e47439
Cesar William <cesarwbr@gmail.com>
2016-05-04 14:18:48 -0300

    Add sequence tests

--- test/test.js
+++ test/test.js
@@ -16,3 +16,26 @@ describe('Testing next value', () => {
     expect(random.nextDouble()).to.be.a('number');
   });
 });
+
+describe('Testing sequence numbers', () => {
+  it('Should chose one item of array', () => {
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    const result = random.choice(arr);
+
+    expect(arr.indexOf(result)).to.be.above(-1);
+  });
+
+  it('Should return the same length of array after shuffle', () => {
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    const result = random.shuffle(arr);
+
+    expect(result.length).to.equal(arr.length);
+  });
+
+  it('Should return a subset of an array', () => {
+    const arr = ['a', 'b', 'c', 'd', 'e'];
+    const result = random.sample(arr, 3);
+
+    expect(result.length).to.equal(3);
+  });
+});
